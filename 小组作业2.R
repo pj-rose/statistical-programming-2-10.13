05cc9ab55f2a3165ad281c5a75624a5e35140fb2
pj-rose <114032783+pj-rose@users.noreply.github.com>
2022-10-13 20:01:30 +0100

    Add files via upload

---
+++ 小组作业2.R
@@ -0,0 +1,24 @@
+
+
+Pone<-function(n, k, strategy,nreps)
+{ p<-rep(0,2*n) #prisoner
+  card<-rep(n,2*n)
+  if (strategy==1)
+    {
+    
+    
+    }
+    
+  if (strategy==2)
+    {
+    
+    
+    }
+  
+  if(strategy==3)
+    {
+    
+    
+    }
+  
+}
